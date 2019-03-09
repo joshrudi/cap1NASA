2515b259b7f50a604d0e6566f63403a9d6b64fae
Joshua Rudaitis <joshr@Joshuas-MacBook-Air.local>
2019-03-09 01:35:26 -0500

    heroku update, updated server js to load app

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+var express = require('express');
+var app = express();
+var path = require('path');
 
-var app = require('app');
-var server = app.start();
+app.use(express.static(path.join(__dirname)));
+app.use("/assets", express.static(__dirname));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
