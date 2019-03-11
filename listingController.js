77274c14c100195a5b5025227c213822532d1797
Joshua Rudaitis <joshr@Joshuas-MacBook-Air.local>
2019-03-11 06:15:24 -0400

    updated navigation features, added filters by date, media and location, allow user to view raw data

--- listingController.js
+++ listingController.js
@@ -3,13 +3,19 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
 
     $scope.listings = [];   //contains search results of current page
     $scope.nav = [];        //contains next and/or prev page links
-    $scope.file = [];       //contains the links to a given items files (.mp4/.png/etc...)
+    $scope.hits = [];       //contains the number of results
+    $scope.curPage = 0;     //stores the current page
+    $scope.curEndRange = 0; //stores the current end of the range
+
 
     /* loads listings with a default view so visitors have something to see by default */
     /* search using query, then bind to scope */
       Listings.search("earth").then(function(response) {
         $scope.listings = response.data.collection.items;
         $scope.nav = response.data.collection.links;
+        $scope.hits = response.data.collection.metadata.total_hits;
+        if ($scope.hits - (($scope.curPage+1)*100) < 0) $scope.curEndRange = $scope.hits;
+        else $scope.curEndRange = (($scope.curPage+1)*100);
         console.log($scope.listings);
       }, function(error) {
         console.log('Unable to retrieve listings:', error);
@@ -18,9 +24,13 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
     $scope.search = function() {
       
       /* search using query, then bind to scope */
-      Listings.search($scope.query).then(function(response) {
+      Listings.search($scope.query, $scope.startDate, $scope.endDate, $scope.isImage, $scope.isVideo, $scope.isAudio, $scope.location).then(function(response) {
         $scope.listings = response.data.collection.items;
         $scope.nav = response.data.collection.links;
+        $scope.hits = response.data.collection.metadata.total_hits;
+        $scope.curPage = 0;
+        if ($scope.hits - (($scope.curPage+1)*100) < 0) $scope.curEndRange = $scope.hits;
+        else $scope.curEndRange = (($scope.curPage+1)*100);
         console.log($scope.listings);
       }, function(error) {
         console.log('Unable to retrieve listings:', error);
@@ -36,6 +46,9 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
         Listings.getLink($scope.nav[0].href).then(function(response) {
           $scope.listings = response.data.collection.items;
           $scope.nav = response.data.collection.links;
+          $scope.curPage = $scope.curPage - 1;
+          if ($scope.hits - (($scope.curPage+1)*100) < 0) $scope.curEndRange = $scope.hits;
+          else $scope.curEndRange = (($scope.curPage+1)*100);
           console.log($scope.listings);
         }, function(error) {
           console.log('Unable to retrieve listings:', error);
@@ -52,6 +65,9 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
         Listings.getLink($scope.nav[0].href).then(function(response) {
           $scope.listings = response.data.collection.items;
           $scope.nav = response.data.collection.links;
+          $scope.curPage = $scope.curPage + 1;
+          if ($scope.hits - (($scope.curPage+1)*100) < 0) $scope.curEndRange = $scope.hits;
+          else $scope.curEndRange = (($scope.curPage+1)*100);
           console.log($scope.listings);
         }, function(error) {
           console.log('Unable to retrieve listings:', error);
@@ -65,6 +81,9 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
         Listings.getLink($scope.nav[1].href).then(function(response) {
           $scope.listings = response.data.collection.items;
           $scope.nav = response.data.collection.links;
+          $scope.curPage = $scope.curPage + 1;
+          if ($scope.hits - (($scope.curPage+1)*100) < 0) $scope.curEndRange = $scope.hits;
+          else $scope.curEndRange = (($scope.curPage+1)*100);
           console.log($scope.listings);
         }, function(error) {
           console.log('Unable to retrieve listings:', error);
@@ -74,7 +93,49 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
 
     $scope.getFile = function(type, url) {
 
+        /* search using url, then bind to scope */
+        Listings.getLink(url).then(function(response) {
 
+          console.log(type);
+          console.log(response);
+
+          if (type === "image") {
+
+              for (var i = 0; i < response.data.length; i++) {
+
+                if (response.data[i].search("orig.jpg") != -1) {
+
+                window.open(response.data[i]);
+                i = response.data.length;
+              }
+            }
+          }
+          else if (type === "video") {
+
+              for (var i = 0; i < response.data.length; i++) {
+
+                if (response.data[i].search("mp4") != -1) {
+
+                  window.open(response.data[i]);
+                  i = response.data.length;
+              }
+            }
+          }
+          else if (type === "audio") {
+
+            for (var i = 0; i < response.data.length; i++) {
+
+                  if (response.data[i].contains("orig.wav")) {
+
+                  window.open(response.data[i]);
+                  i = response.data.length;
+                }
+              }
+            }
+
+        }, function(error) {
+          console.log('Unable to retrieve listings:', error);
+        });
     }
   }
 ]);
--- listingFactory.js
+++ listingFactory.js
@@ -1,7 +1,37 @@
 angular.module('listings').factory('Listings', function($http) {
   var methods = {
-    search: function(query) {
-    	return $http.get('https://images-api.nasa.gov/search?q=' + query);
+    search: function(query, startDate, endDate, isImage, isVideo, isAudio, location) {
+
+    	query = encodeURIComponent(query.trim());  // encode for URL
+
+		// below filter conditions makes no sense, assume user got confused
+    	if (startDate != undefined && endDate != undefined && startDate > endDate) [startDate, endDate] = [endDate, startDate];  
+
+
+    	var search = 'https://images-api.nasa.gov/search?q=' + query;
+    	var media = '';
+
+    	if (startDate != undefined) search += '&year_start=' + startDate;
+    	if (endDate   != undefined)	search += '&year_end=' + endDate;
+    	if (location  != undefined) search += '&location=' + location;
+
+
+    	// the great thing about how this (below) code works, is that if the user has all 3 boxes (image, video, audio) UNchecked,
+    	// then it will just search using all three as if they were checked.  There's no point in searching for nothing.
+    	if (isImage === true) media += 'image';
+    	if (isVideo === true) {
+    		if (media.length > 0) media += ',';
+    		media += 'video';
+    	}
+    	if (isAudio === true) {
+    		if (media.length > 0) media += ',';
+    		media += 'audio';
+    	}
+
+    	if (media.length > 0) search += '&media_type=' + media;
+
+    	
+    	return $http.get(search);
     },
 
     getLink: function(link) {
